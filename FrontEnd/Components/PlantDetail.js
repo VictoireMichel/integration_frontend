cb781a4b0b94b1d07ed205c35d9edd101ad84ea6
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-12 00:09:22 +0200

    US 4

--- FrontEnd/Components/PlantDetail.js
+++ FrontEnd/Components/PlantDetail.js
@@ -31,11 +31,19 @@ class PlantDetail extends React.Component {
                     <Image
                         style={styles.image}
                         source={require('../assets/Images/persil.png')}
+
                     />
 
-                    <Text style={styles.description_text}>Description {tempRealId.nom} </Text>
-                    <Text style={styles.description_text}>Utilisation {tempRealId.croissance}</Text>
-                    <Text style={styles.description_text}>Info{tempRealId.description}</Text>
+                    <Text style={styles.description_title}>Croissance(jours)</Text>
+                    <Text style={styles.description_text}>{tempRealId.croissance}</Text>
+                    <Text style={styles.description_title}>Description</Text>
+                    <Text style={styles.description_text}>{tempRealId.description}</Text>
+                    <Text style={styles.description_title}>Sol</Text>
+                    <Text style={styles.description_text}>{tempRealId.sol}</Text>
+                    <Text style={styles.description_title}>Luminosité demandée</Text>
+                    <Text style={styles.description_text}>{tempRealId.luminosité}</Text>
+                    <Text style={styles.description_title}>Entretien</Text>
+                    <Text style={styles.description_text}>{tempRealId.entretien}</Text>
                 </View>
             )
         }
@@ -50,7 +58,6 @@ class PlantDetail extends React.Component {
         return (
             <ScrollView>
                 <View style={styles.main_container}>
-                    <Text style={styles.text}>Voici la plant n° {idPlant}</Text>
                     {this._displayPlant()}
                 </View>
             </ScrollView>
@@ -77,10 +84,14 @@ const styles = StyleSheet.create({
         color: '#666666',
         margin: 5,
         marginBottom: 15,
-        textAlign: "center"
+        textAlign: "center",
+        fontSize: 15
     },
     nomPlante: {
-        fontSize: 25
+        fontSize: 30
+    },
+    description_title: {
+        fontSize: 20
     }
 })
 
--- FrontEnd/Components/PlantsDatas.js
+++ FrontEnd/Components/PlantsDatas.js
@@ -2,68 +2,97 @@ const PlantsList = [
     {
         id: 1,
         nom: 'Persil',
+        description: "Le persil est une plante bisannuelle de 25 à 80 cm de haut, très aromatique au froissement, à odeur caractéristique.\n" +
+            "\n" +
+            "Les tiges sont striées et les feuilles sont glabres. Les feuilles d'un vert luisant sont doublement divisées, surtout celles de la base, les feuilles supérieures ayant souvent seulement trois lobes étroits et allongés. Les fleurs, d'une couleur jaune verdâtre tirant sur le blanc en pleine floraison, sont groupées en ombelles composées comprenant huit à vingt rayons. Les ombellules sont munies d'un involucelle à nombreuses bractées. La racine allongée de type pivotant est assez développée. Elle est jaunâtre, d'odeur forte et aromatique.",
         croissance: '25',
-        saison: '1',
-        description: "C'est une plante verte hé oui"
+        entretien: 'Récolter toutes les 3 à 4 semaines',
+        sol: 'riche',
+        luminosité: '1(ensoleillé)',
+        imageurl:   '../images/persil.png'
     },
     {
         id: 2,
         nom: 'Origan',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "Les plantes atteignent généralement une taille variant entre 30 et 80 cm.\n" +
+            "\n" +
+            "Les tiges rouges, à section carrée, sont velues avec des feuilles arrondies, vertes, légèrement dentées.\n" +
+            "\n" +
+            "Les fleurs sont roses ou pourpres, et sont regroupées en petits panicules.",
+        imageurl: '../images/origan.png'
     },
     {
         id: 3,
         nom: 'Thym',
+        description: "Le Thym est un sous-arbrisseau à racines ligneuses, rameuses, tortueuses. Les tiges, de 10 à 30 centimètres, sont ligneuses à la base, herbacées au sommet, très diffuses. Les feuilles, opposées, sessiles, très petites, ovales, lancéolées, ont un bord rouge en dessous. Les fleurs, roses, visibles de juin à octobre, sont réunies en glomérules ; l'ensemble constitue des grappes terminales feuillées. Le fruit se compose de quatre akènes ovoïdes arrondis."
+        ,
+        entretien: ' Garder votre plante dans une pièce lumineuse ou sur le rebord d’une fenêtre de façon à ce qu’elle profite du soleil toute la journée. La terre du pot doit être poreuse, pas trop argileuse et bien drainée.L’arrosage du thym en pot doit être régulier, notamment la première année de culture ou en cas de sécheresse prolongée. Attention toutefois à ne pas trop arroser : le thym craint les excès d’humidité.',
         croissance: '28',
-        saison: '1',
-        description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.\n" +
-            "\n" +
-            "Pourquoi l'utiliser?\n" +
-            "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes)."
+        sol: 'très bien drainé',
+        luminosité: '2(très ensoleillé)',
+        imageurl: '../images/thym.png'
     },
     {
         id: 4,
         nom: 'Ail',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "Elle est composée d’une tige creuse et assez grande, de nombreuses feuilles engainant le bas de cette tige, de fleurs blanches ou rosées groupées en ombelles, et d’un bulbe (ou ‘’tête’’). Ce dernier est lui-même constitué de 10 à 16 bulbilles arquées appelées caïeux (ou \"gousses\" d'ail), blancs ou rosés, réunis entre eux et entourés d’une membrane blanche. \n" +
+            "\n" +
+            "Il a une odeur forte et un goût âcre. Cette odeur pénétrante et désagréable est due à la présence d’une molécule qui ne préexiste pas dans la plante, mais qui se forme lors de la coupure ou du broyage du bulbe.",
+        imageurl: '../images/ail.png'
     },
     {
         id: 5,
         nom: 'Oignon',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "L'oignon est une espèce herbacée, vivace par son bulbe unique, cultivée comme une annuelle ou bisannuelle (floraison la deuxième année). C'est une plante haute de 60 à 100 cm, dont les feuilles de couleur verte sont cylindriques, creuses (ce qui distingue cette espèce du poireau et de l'ail, autres espèces cultivées appartenant aussi au genre Allium). La tige florale dressée est également creuse. Elle présente un renflement vers sa base.\n" +
+            "\n" +
+            "Le bulbe est relativement gros, de forme sphérique, parfois plus ou moins aplatie.\n" +
+            "\n" +
+            "Les fleurs petites (de 4 à 5 mm de large), de couleur blanche ou verte, sont regroupées en une ombelle sphérique, en position terminale sur la tige. Les fleurs ont une symétrie trimère, à trois sépales, trois pétales et six étamines. L'ovaire unique est divisé en trois loges. Le fruit est une capsule s'ouvrant par trois valves, libérant chacune généralement deux graines. On peut compter environ 600 fleurs par ombelle.\n" +
+            "\n" +
+            "Chez certaines variétés, il arrive que des bulbilles se développent à la place des fleurs.\n" +
+            "\n" +
+            "Les graines sont noires, anguleuses. Il y a environ 250 graines par gramme. La durée de germination est faible (2 ans).",
+        imageurl: '../images/oignon.png'
     },
     {
         id: 6,
         nom: 'Ciboulette',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "La ciboulette (Allium schoenoprasum L.) est une plante vivace originaire du bassin méditerranéen. Elle fait partie de la famille des Alliacées, tout comme l'ail et l'oignon. Elle est communément utilisée comme condiment. Son feuillage cylindrique et creux, vert foncé et très linéaire permet de constituer de jolies bordures au potager ou dans les allées. Sa floraison en ombelles couleur lilas ajoute encore à son attrait ornemental. On trouve aussi des ciboulettes à fleurs blanches ou même rose pourpré foncé comme la variété 'Forescate' par exemple.",
+        imageurl: '../images/ciboulette.png'
     },
     {
         id: 7,
-        nom: 'Echalotte',
+        nom: 'Echalote',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "C'est une plante vivace par son bulbe, mais cultivée comme annuelle. Elle forme une touffe haute de 20 à 30 cm de feuilles cylindriques creuses. Le bulbe planté comporte plusieurs points végétatifs, qui, après la plantation, donneront naissance à de nouveaux bulbes-fils formant une touffe. L'inflorescence est, comme chez toutes les espèces du genre Allium, une ombelle sphérique. Les graines sont petites et noires.\n" +
+            "\n" +
+            "La plante est très voisine de l'oignon (Allium cepa var. cepa) mais présente un nombre plus important de points végétatifs par bulbe ; ces points sont visibles en coupant transversalement un bulbe. La saveur de l'échalote est également plus marquée que celle de l'oignon.",
+        imageurl: '../images/echalote.png'
     },
     {
         id: 8,
         nom: 'Cerfeuil',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "Anthriscus cerefolium est une plante fine et menu. Germant au printemps, il montre une croissance rapide, puis finit toujours par fleurir durant l’été. Ses feuilles ont un délicat limbe mince et très découpé, rappelant une fougère. Elles ont un parfum et un gout très caractéristique, appétissant. Les tiges s’élèvent jusqu’à 50 cm de hauteur pour fleurir en plusieurs ombelles, regroupant des petites fleurs blanches, formant un assez joli bouquet.",
+        imageurl: '../images/cerfeuil.png'
     },
     {
         id: 9,
         nom: 'Fenouil',
         croissance: '25',
         saison: '1',
-        description: "C'est une plante verte hé oui"
+        description: "Plante aromatique, vivace, de la famille des ombellifères. Originaire d'Europe méridionnale, cultivé dès le moyen âge en Italie, on la trouve dans toute l'Europe centrale, en Asie occidentale et en Afrique du nord. La racine est ligneuse, la tige buissonnante, et peut monter à 1,5m.",
+        imageurl: '../images/fenouil.png'
     }
 ];
 
