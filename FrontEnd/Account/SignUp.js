d57014bc0e9f6f63a2e91fea67b5536c75769441
Robin <robin@castermane.be>
2020-10-15 19:29:28 +0200

    US 9.1 fini

--- FrontEnd/Account/SignUp.js
+++ FrontEnd/Account/SignUp.js
@@ -1,51 +1,102 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import {faLockOpen, faMailBulk, faLock} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+
+const mailIcon =  () => {
+    return <FontAwesomeIcon icon={ faMailBulk } />
+}
+const passwordIcon =  () => {
+    return <FontAwesomeIcon icon={ faLockOpen } />
+}
+const passwordLockIcon =  () => {
+    return <FontAwesomeIcon icon={ faLock } />
+}
 
 class SignUp extends React.Component{
     render() {
         return(
-            <View style={styles.container}>
-                <View style={styles.container_title}>
-                    <Text style={styles.title_text}>Sign Up</Text>
+            <ScrollView style={styles.container_out}>
+                <View style={styles.container_in}>
+
+
+                    <View style={styles.container_title}>
+                        <Text style={styles.title_text}>Inscription</Text>
+                    </View>
+
+
+                    <View style={styles.container_input}>
+                        <View style={styles.icon_inputText}>{mailIcon()}</View>
+                        <TextInput
+                            placeholder="Email"
+                            style={styles.input_text}
+                        />
+                    </View>
+                    <View style={styles.container_input}>
+                        <View style={styles.icon_inputText}>{passwordIcon()}</View>
+                        <TextInput
+                            secureTextEntry={true}
+                            placeholder="Password"
+                            style={styles.input_text}
+                        />
+                    </View>
+                    <View style={styles.container_input}>
+                        <View style={styles.icon_inputText}>{passwordLockIcon()}</View>
+                        <TextInput
+                            secureTextEntry={true}
+                            placeholder="Password again"
+                            style={styles.input_text}
+                        />
+                    </View>
+
+
+                    <TouchableOpacity style={styles.container_validate}
+                                      onPress={()=> {this.props.navigation.navigate('Acceuil')}}>
+                        <Text style={styles.validate_text}>S'inscrire</Text>
+                    </TouchableOpacity>
+
+
+
+                    <View style={styles.container_Bottom}>
+                        <View style={styles.container_bottomText}>
+                            <Text style={styles.text_bottom}>Vous avez déjà un compte ? </Text>
+                        </View>
+                        <TouchableOpacity style={styles.container_bottomButton}
+                                          onPress={()=> {this.props.navigation.navigate('SignIn')}}>
+                            <Text style={styles.text_button}>
+                                Connexion
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+
                 </View>
-
-                <View style={styles.container_input}>
-                    <TextInput
-                        placeholder="Email"
-                        style={styles.input_text}
-                    />
-                </View>
-
-                <View style={styles.container_input}>
-                    <TextInput
-                        placeholder="Password"
-                        style={styles.input_text}
-                    />
-                </View>
-
-                <View style={styles.container_input}>
-                    <TextInput
-                        placeholder="Password again"
-                        style={styles.input_text}
-                    />
-                </View>
-
-                <TouchableOpacity style={styles.container_validate}>
-                    <Text style={styles.validate_text}>Sign Up</Text>
-                </TouchableOpacity>
-
-            </View>
+            </ScrollView>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        height: '100%'
+    container_out: {
+        backgroundColor: '#e2e2e2',
+        height: '100%',
+        flexDirection: 'column'
+    },
+    container_in: {
+        backgroundColor: 'white',
+        height: 420,
+        marginTop: '10%',
+        marginBottom: '10%',
+        width: 310,
+        marginRight: 'auto',
+        marginLeft: 'auto',
+
+        // its for android
+        elevation: 5,
+        borderRadius:10,
     },
     container_title: {
-        marginTop: 50,
+        marginTop: 30,
         marginBottom: 30
     },
     title_text: {
@@ -55,26 +106,33 @@ const styles = StyleSheet.create({
         letterSpacing: 1.15,
     },
     container_input: {
-
-    },
-    input_text: {
+        height: 37,
+        width: 270,
+        paddingLeft: 30,
         marginRight: 'auto',
         marginLeft: 'auto',
         marginTop: 10,
         marginBottom: 10,
+        backgroundColor: '#fff',
+
+        flexDirection: 'row',
         alignItems: 'center',
-        color: "#588B43",
-        height: 37,
-        width: 307,
-        borderRadius: 8,
-        paddingLeft: 30,
-        fontSize: 18,
-        backgroundColor:'#F8F8F8',
+
+
+        borderRadius:10,
         elevation: 5,
         position:'relative'
     },
+    icon_inputText: {
+        paddingRight: 10
+    },
+    input_text: {
+        fontSize: 18,
+        backgroundColor: '#fff',
+        color: '#424242'
+    },
     container_validate: {
-        width: 307,
+        width: 270,
         height: 40,
         marginTop: 25,
         alignItems: 'center',
@@ -82,9 +140,7 @@ const styles = StyleSheet.create({
         marginRight: 'auto',
         marginLeft: 'auto',
         backgroundColor:'#588B43',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
+
         // its for android
         elevation: 5,
         position:'relative',
@@ -93,7 +149,21 @@ const styles = StyleSheet.create({
     validate_text: {
         color: "white",
         fontSize: 18
-    }
+    },
+    container_Bottom: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 15
+    },
+    container_bottomText: {
+
+    },
+    text_bottom: {},
+    container_bottomButton: {},
+    text_button: {
+        color: '#588B43',
+        fontWeight: 'bold'
+    },
 })
 
 export default SignUp
