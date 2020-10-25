d1343963d456e2dd1571b2820f0acb8397010cf6
Robin <robin@castermane.be>
2020-10-25 19:30:41 +0100

    début test

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,25 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './Components/Home'
 import Nav from "./Navigation/Nav";
 import Header from "./Components/Header";
 
-
-
-
-
-
 export default function App() {
   return (
-
       <>
         <Header/>
         <Nav/>
-
       </>
-
   );
 }
-
-const styles = StyleSheet.create({});
