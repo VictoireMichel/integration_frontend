0df166f9552af0f54d1635c91476d3b79670f1f1
Robin <robin@castermane.be>
2020-10-25 19:41:35 +0100

    Revert "Merge branch 'ListeDePlantes' into DevOps"
    This reverts commit 042fc82ee9b11443d73b0218aab78130b5048ec6, reversing
    changes made to 386a15d8a71ba6c4c81e7bce89e39fb14af4eee1.

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import Nav from "./Navigation/Nav";
 import Header from "./Components/Header";
+
 
 export default function App() {
 
   return (
+
       <>
         <Nav/>
       </>
+
   );
 }
+
