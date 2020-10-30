ed4680dfdc7b2ed0fb3f9144ea9998e39247559c
Robin <robin@castermane.be>
2020-10-30 09:31:31 +0100

    Fin navigation 5

--- FrontEnd/Navigation/StackNavigator/HomeNavigation.js
+++ FrontEnd/Navigation/StackNavigator/HomeNavigation.js
@@ -3,10 +3,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MyPlants from '../../Components/PlantList/MyPlants';
 import MyPlantInfo from "../../Components/MyPlantInfo";
 import HomeConnectedWithPot from "../../Components/Home/HomeConnectedWithPot";
+import {useEffect} from "react";
 
 const Stack = createStackNavigator();
 
-function HomeNavigation() {
+function HomeNavigation({ navigation, route }) {
+    useEffect(()=> {
+        if(route.state !== undefined){
+            if(route.state.index > 0){
+                navigation.setOptions({ tabBarVisible: false})
+            }else{
+                navigation.setOptions({ tabBarVisible: true})
+            }
+        }
+    })
+
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeConnectedWithPot} options={{ headerShown: false }}/>
--- FrontEnd/Navigation/StackNavigator/ListPlantsNavigation.js
+++ FrontEnd/Navigation/StackNavigator/ListPlantsNavigation.js
@@ -2,10 +2,20 @@ import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MyPlants from '../../Components/PlantList/MyPlants';
 import PlantDetail from "../../Components/PlantList/PlantDetail";
+import {useEffect} from "react";
 
 const Stack = createStackNavigator();
 
-function ListPlantsNavigation() {
+function ListPlantsNavigation({ navigation, route }) {
+    useEffect(()=> {
+        if(route.state !== undefined){
+            if(route.state.index > 0){
+                navigation.setOptions({ tabBarVisible: false})
+            }else{
+                navigation.setOptions({ tabBarVisible: true})
+            }
+        }
+    })
     return (
         <Stack.Navigator>
             <Stack.Screen name="Liste" component={MyPlants} options={{ headerShown: false }}/>
