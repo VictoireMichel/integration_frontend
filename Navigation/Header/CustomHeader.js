5a63f190006afd0dbfeff3e4d58afb035726a4df
Victoire Michel <v.michel@students.ephec.be>
2020-11-06 22:42:03 +0100

    icone menu corrigée

--- Navigation/Header/CustomHeader.js
+++ Navigation/Header/CustomHeader.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Body, Button, Header, Icon, Left, Title } from "native-base";
 import { Text } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 export default class CustomHeader extends React.Component {
   render () {
     return (
       <Header style={{ backgroundColor: "#588B43" }} androidStatusBarColor="#588B43">
         <Left>
-          <Button transparent><Icon name="menu" style={{ color: "#fff" }} onPress={() => this.props.nav.openDrawer()}/></Button>
+            <Button transparent>
+                <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} size={20} onPress={() => this.props.nav.openDrawer()}/>
+            </Button>
         </Left>
         <Body >
-          <Text style={{ color: "#f1f1f1", fontSize: 20, paddingLeft: 40 }}>eGrow</Text>
+          <Text style={{ color: "#f1f1f1", fontSize: 20, paddingLeft: 55 }}>eGrow</Text>
         </Body>
       </Header>
     );
