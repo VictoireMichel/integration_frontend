57b81d4b9491bd4bb09f82974c747c334ae9bb0d
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2020-11-08 16:44:13 +0100

    Update SignUpStep2.js

--- Account/SignUpStep2.js
+++ Account/SignUpStep2.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
-import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
-import {faMailBulk, faLockOpen, faLock, faArrowCircleLeft, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faMailBulk, faLockOpen, faLock, faArrowCircleLeft, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import CustomHeader from "../Navigation/Header/CustomHeader";
 import TextInput from 'react-native-input-validator';
 
 
-class SignIn extends React.Component{
+class SignIn extends React.Component {
 
     constructor(props) {
         super(props);
@@ -16,53 +16,75 @@ class SignIn extends React.Component{
             email: '',
             password1: '',
             password2: '',
-            filedField: false
+            filedField: false,
+            validField: false
         }
     }
 
-    _testFiledField(){
-        if(this.state.name !== '' & this.state.firstName !== '' & this.state.email !== ''
-        & this.state.password1 !== '' & this.state.password2 !== ''){
-            this.setState({filedField: true})
+    _testFiledField() {
+        if (this.state.name !== '' & this.state.firstName !== '' & this.state.email !== ''
+            & this.state.password1 !== '' & this.state.password2 !== '') {
+            this.setState({ filedField: true });
         }
     }
-    _testValidData(){
+    _testValidData() {
         this._testFiledField();
 
-        if(this.input.isValid() & this.state.password1 === this.state.password2 & this.state.filedField){
+        if (this.input.isValid() & this.state.password1 === this.state.password2 & this.state.filedField) {
             console.log('Champs valide');
-        }else if(!this.input.isValid()){
+            this.setState({ validField: true });
+        } else if (!this.input.isValid()) {
             alert('adresse Email non valide');
-        }else if(this.state.password1 !== this.state.password2) {
+        } else if (this.state.password1 !== this.state.password2) {
             alert('Mot de passe non identique');
-        }else {
+        } else {
             alert('Champ(s) non complet');
         }
         this._postDataApi()
     }
 
-    _postDataApi(){
-        alert('To Do : PostMethod')
+    _postDataApi() {
+        //alert('To Do : PostMethod')
+        if (this.state.validField) {
+
+            fetch("http://51.77.203.95:3000/users/signin", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "mail": "robin@test.com",
+                    "password": "user1234",
+                    "firstName": "robin",
+                    "lastName": "castermane",
+                    "learningMode": false
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                })
+        }
     }
 
     render() {
-        return(
+        return (
             <ScrollView>
 
-                <CustomHeader nav={this.props.navigation}/>
+                <CustomHeader nav={this.props.navigation} />
 
                 <View style={styles.main_container}>
 
                     <View style={styles.step}>
-                        <View style={styles.circleDone}/>
-                        <View style={styles.line}/>
-                        <View style={styles.circleDone}/>
+                        <View style={styles.circleDone} />
+                        <View style={styles.line} />
+                        <View style={styles.circleDone} />
                     </View>
 
                     <View><Text style={styles.title_Screen}>Inscription</Text></View>
 
                     <View style={styles.inputView}>
-                        <View style={styles.icon_inputText}><FontAwesomeIcon icon= {faMailBulk} /></View>
+                        <View style={styles.icon_inputText}><FontAwesomeIcon icon={faMailBulk} /></View>
                         <TextInput
                             onRef={(r) => {
                                 this.input = r;
@@ -70,30 +92,30 @@ class SignIn extends React.Component{
                             placeholder="Email"
                             type="email"
                             value={this.state.email}
-                            onChangeText={(text => this.setState({email:text}))}
+                            onChangeText={(text => this.setState({ email: text }))}
                             style={styles.inputText}>
 
                         </TextInput>
                     </View>
 
                     <View style={styles.inputView}>
-                        <View style={styles.icon_inputText}><FontAwesomeIcon icon= {faLockOpen} /></View>
+                        <View style={styles.icon_inputText}><FontAwesomeIcon icon={faLockOpen} /></View>
                         <TextInput
                             secureTextEntry={true}
                             placeholder="Password"
                             value={this.state.password1}
-                            onChangeText={(text => this.setState({password1:text}))}
+                            onChangeText={(text => this.setState({ password1: text }))}
                             style={styles.inputText}>
 
                         </TextInput>
                     </View>
                     <View style={styles.inputView}>
-                        <View style={styles.icon_inputText}><FontAwesomeIcon icon= {faLock} /></View>
+                        <View style={styles.icon_inputText}><FontAwesomeIcon icon={faLock} /></View>
                         <TextInput
                             secureTextEntry={true}
                             placeholder="Password again"
                             value={this.state.password2}
-                            onChangeText={(text => this.setState({password2:text}))}
+                            onChangeText={(text => this.setState({ password2: text }))}
                             style={styles.inputText}>
 
                         </TextInput>
@@ -103,13 +125,13 @@ class SignIn extends React.Component{
 
                     <View style={styles.valid_container}>
                         <View>
-                            <TouchableOpacity style={styles.step_button} onPress = {() => {this.props.navigation.goBack()}}>
+                            <TouchableOpacity style={styles.step_button} onPress={() => { this.props.navigation.goBack() }}>
                                 <Text style={styles.stepText}>Retour</Text>
                             </TouchableOpacity>
                         </View>
 
                         <View>
-                            <TouchableOpacity style={styles.step_button} onPress = {() => this._testValidData()}>
+                            <TouchableOpacity style={styles.step_button} onPress={() => this._testValidData()}>
                                 <Text style={styles.stepText}>S'inscrire</Text>
                             </TouchableOpacity>
                         </View>
@@ -131,34 +153,34 @@ const styles = StyleSheet.create({
     },
     title_Screen: {
         marginTop: 30,
-        fontSize:35,
-        color:'green',
+        fontSize: 35,
+        color: 'green',
         marginBottom: 25
     },
-    step:{
-        flexDirection:'row',
-        marginTop:25
+    step: {
+        flexDirection: 'row',
+        marginTop: 25
     },
-    circleDone:{
-        width:30,
-        height:30,
-        backgroundColor:'green',
-        borderRadius:30,
+    circleDone: {
+        width: 30,
+        height: 30,
+        backgroundColor: 'green',
+        borderRadius: 30,
         elevation: 5,
-        position:'relative'
+        position: 'relative'
     },
-    line:{
-        width:100,
-        height:5,
-        backgroundColor:'green',
-        borderRadius:30,
-        marginTop:12,
+    line: {
+        width: 100,
+        height: 5,
+        backgroundColor: 'green',
+        borderRadius: 30,
+        marginTop: 12,
         elevation: 5,
-        position:'relative',
+        position: 'relative',
         marginLeft: 10,
         marginRight: 10
     },
-    inputView:{
+    inputView: {
         height: 37,
         width: 270,
         paddingLeft: 30,
@@ -169,33 +191,33 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         flexDirection: 'row',
         alignItems: 'center',
-        borderRadius:10,
+        borderRadius: 10,
         elevation: 5,
-        position:'relative'
+        position: 'relative'
     },
     inputText: {
         padding: 10,
-        marginBottom:20,
-        borderWidth:0,
+        marginBottom: 20,
+        borderWidth: 0,
     },
-    step_button:{
-        width:140,
-        backgroundColor:'#588B43',
-        borderRadius:15,
-        height:50,
-        marginTop:40,
-        marginBottom:10,
+    step_button: {
+        width: 140,
+        backgroundColor: '#588B43',
+        borderRadius: 15,
+        height: 50,
+        marginTop: 40,
+        marginBottom: 10,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
-        margin:10
+        margin: 10
     },
     stepText: {
         color: 'white',
         fontSize: 20,
     },
-    valid_container:{
-        flexDirection:'row'
+    valid_container: {
+        flexDirection: 'row'
     }
 })
 
