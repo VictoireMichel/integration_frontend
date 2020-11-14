d600d1f95804d7e8480c0cd392c31f203c9de4fe
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-11-14 23:33:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from "react";
 import MyDrawer from "./Navigation/DrawerNavigation";
 import SplashScreen from "react-native-splash-screen";
 import {useEffect} from "react";
+import {Provider} from 'react-redux'
+import Store from "./redux/store"
 
 export default function App() {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  return <MyDrawer />;
+  return (
+      <Provider store={Store}>
+          <MyDrawer />
+      </Provider>
+  );
 }
