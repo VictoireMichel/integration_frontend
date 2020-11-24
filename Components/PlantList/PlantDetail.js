dd9fa846667ff4dc3c6cd4d774e1e674efcbf209
CastermaneRobin <55954496+CastermaneRobin@users.noreply.github.com>
2020-11-24 13:56:04 +0100

    Update US 4
    Ajout de "exposition" à la place de "luminosité demandé" et ajout d'une image

--- Components/PlantList/PlantDetail.js
+++ Components/PlantList/PlantDetail.js
@@ -220,19 +220,40 @@ class PlantDetail extends React.Component {
     const plant = this.state.plant;
     if (plant.length > 0) {
       return (
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}>
-          <Text style={{ fontSize: 18, color: "#f1f1f1", fontFamily: "serif" }}>
-            Luminosité demandée
-          </Text>
-          <Text style={{ fontSize: 14, color: "#f1f1f1", fontFamily: "serif" }}>
-            {plant[0].luminosity}
-          </Text>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            <Image
+              style={{ width: 60, height: 60 }}
+              source={require("../../Picture/soleil.png")}
+            />
+          </View>
+
+          <View style={{ flex: 1, flexDirection: "column", marginRight: 18 }}>
+            <View
+              style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+              <Text style={{ fontSize: 18, color: "#f1f1f1", fontFamily: "serif" }}>Exposition</Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "flex-start",
+              }}>
+              <ScrollView style={{ marginBottom: 5 }}>
+                <Text style={{ fontSize: 14, color: "#f1f1f1", fontFamily: "serif" }}>
+                  {(plant[0].luminosity === 0) ? (<Text>Ombre</Text>) : (plant[0].luminosity === 1) ?
+                    (<Text>Mi-ombre</Text>) :
+                    (<Text>Soleil</Text>)}
+                </Text>
+              </ScrollView>
+            </View>
+          </View>
         </View>
       );
     }
