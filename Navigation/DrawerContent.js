3f8af139a6bbfcb6567394aadf85c5bc471078f1
Victoire Michel <v.michel@students.ephec.be>
2020-11-26 09:59:35 +0100

    refactor

--- Navigation/DrawerContent.js
+++ Navigation/DrawerContent.js
@@ -1,48 +1,58 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faQuestionCircle, faUser } from "@fortawesome/free-solid-svg-icons";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faQuestionCircle, faUser} from "@fortawesome/free-solid-svg-icons";
 import ToggleSwitch from "toggle-switch-react-native";
 import store from "../redux/store";
-import { logOut } from "../GetDataFromApi/GetDataFromApi"
+import {logOut} from "../GetDataFromApi/GetDataFromApi";
 
 export default function DrawerContent(props) {
-
-
-
   return (
-    <View style={styles.slide} forceInset={{ top: "always" }}>
+    <View style={styles.slide} forceInset={{top: "always"}}>
       <View style={styles.header}>
         <FontAwesomeIcon style={styles.user_icon} icon={faUser} size={80} />
       </View>
       <View style={styles.text_container}>
-        <Text style={styles.text} onPress={() => {
-          props.navigation.navigate("Accueil");
-        }}>
+        <Text
+          style={styles.text}
+          onPress={() => {
+            props.navigation.navigate("Accueil");
+          }}>
           Accueil
         </Text>
 
-
-        {store.getState().isLogged.isLoggedIn ? (<Text key='1' style={styles.text} onPress={() => {
-          store.dispatch({ type: "SET_ID", value: null });
-          store.dispatch({ type: "LOGIN", value: false });
-          logOut();
-          props.navigation.navigate("Accueil"); // Déconnexion
-        }}>
-          Deconnexion
-        </Text>) : ([<Text key='2' style={styles.text} onPress={() => {
-          props.navigation.navigate("Connexion");
-        }}>
-          Se connecter
-        </Text>,
-        <Text key='3' style={styles.text} onPress={() => {
-          props.navigation.navigate("Enregistrement");
-        }}>
-          S'inscrire
+        {store.getState().isLogged.isLoggedIn ? (
+          <Text
+            key="1"
+            style={styles.text}
+            onPress={() => {
+              store.dispatch({type: "SET_ID", value: null});
+              store.dispatch({type: "LOGIN", value: false});
+              logOut();
+              props.navigation.navigate("Accueil"); // Déconnexion
+            }}>
+            Deconnexion
           </Text>
-        ])}
-
-
+        ) : (
+          [
+            <Text
+              key="2"
+              style={styles.text}
+              onPress={() => {
+                props.navigation.navigate("Connexion");
+              }}>
+              Se connecter
+            </Text>,
+            <Text
+              key="3"
+              style={styles.text}
+              onPress={() => {
+                props.navigation.navigate("Enregistrement");
+              }}>
+              S'inscrire
+            </Text>,
+          ]
+        )}
       </View>
       <View style={styles.end}>
         <View style={styles.switch}>
@@ -51,19 +61,21 @@ export default function DrawerContent(props) {
             onColor="#588B43"
             offColor="grey"
             label="Mode"
-            labelStyle={{ color: "black", fontWeight: "900", fontSize: 16 }}
+            labelStyle={{color: "black", fontWeight: "900", fontSize: 16}}
             size="medium"
-            onToggle={isOn => console.log("changed to : ", isOn)}
+            onToggle={(isOn) => console.log("changed to : ", isOn)}
           />
         </View>
         <View style={styles.help_container}>
           <Text style={styles.help_text}>
-            <FontAwesomeIcon style={styles.help_icon} icon={faQuestionCircle} size={16} />
-            <View style={{ width: 5 }}>
-            </View>
-                        Aide
+            <FontAwesomeIcon
+              style={styles.help_icon}
+              icon={faQuestionCircle}
+              size={16}
+            />
+            <View style={{width: 5}} />
+            Aide
           </Text>
-
         </View>
       </View>
     </View>
@@ -74,17 +86,17 @@ const styles = StyleSheet.create({
   slide: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor: "#F0F0F0"
+    backgroundColor: "#F0F0F0",
   },
   header: {
     backgroundColor: "#588B43",
     width: 280,
     height: 150,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   text_container: {
-    marginTop: 20
+    marginTop: 20,
   },
   text: {
     backgroundColor: "#284F35",
@@ -95,31 +107,27 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     alignItems: "center",
-    textAlign: "center"
+    textAlign: "center",
   },
   user_icon: {
-    color: "#fff"
+    color: "#fff",
   },
   end: {
     flex: 1,
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   switch: {
     flex: 0,
     alignItems: "center",
-    marginBottom: 20
+    marginBottom: 20,
   },
   help_container: {
     flex: 0,
     alignItems: "center",
-    marginBottom: 20
+    marginBottom: 20,
   },
   help_text: {
-    fontSize: 16
-
+    fontSize: 16,
   },
-  help_icon: {
-
-  },
-
+  help_icon: {},
 });
