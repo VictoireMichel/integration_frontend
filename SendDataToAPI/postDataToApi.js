41f1381f266bfae979fe97e627cd4bb6bba46448
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-12-01 13:32:58 +0100

    Update postDataToApi.js

--- SendDataToAPI/postDataToApi.js
+++ SendDataToAPI/postDataToApi.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 export function postPotDatatoApi (name, needWater, dayCount,plantId, userId) {
-    const url = "https://pi2-ephec.herokuapp.com/pots/add?name="+ name + "&needWater="+ needWater + "&dayCount=" + dayCount + "&plantId="+ plantId +  "&userId=" + userId;
+    const url = "https://pi2-ephec.herokuapp.com/pots/one";
     fetch(url, {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json'
         },
-        body: {
+        body: JSON.stringify({
             name: name,
             needWater: needWater,
             dayCount: dayCount,
             plantId: plantId,
             userId: userId,
-        }
+        })
     }).then((response) => response.json())
         .then((json) => {
             console.log(json);
