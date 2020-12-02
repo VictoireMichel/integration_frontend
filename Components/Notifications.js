e3d5013d095077fffe9c6cf286b70e4b8206e68e
Igor Vandervelden <34266875+Gri097@users.noreply.github.com>
2020-12-02 22:29:13 +0100

    Notif

--- Components/Notifications.js
+++ Components/Notifications.js
@@ -1,6 +1,47 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 
+class PotDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idPot: null,
+      idPlant: null,
+      userId: null,
+      isLoading: true,
+      infosPots: [],
+      infosData: [],
+      infosPlant: [],
+    };
+  }
+
+  componentDidMount() {
+
+    console.log(this.state)
+
+    getPotsByUserIDFromApi(store.getState().storeUserId.id).then((data) => {
+      this.setState({
+        infosPots: data[0],
+        isLoading: false,
+      });
+      getPlantsByIDFromApi(this.state.infosPots.plantId).then((data) => {
+        this.setState({
+          infosPlant: data,
+          isLoading: false,
+        });
+      });
+      getDataByIDFromApi(1).then((data) => { // this.state.infosPots.id
+        this.setState({
+          infosData: data,
+          isLoading: false,
+        });
+      });
+    }).catch(error => console.log('erreur getPotByUserIdFromApi', error));
+  }
+}
+
+
+
 class Notifications extends React.Component {
   render() {
     return (
@@ -23,5 +64,12 @@ const styles = StyleSheet.create({
     color: "red",
   },
 });
+
+const mapStateToProps = (state) => {
+  return {
+    id: state.storeUserId.id,
+    isLoggedIn: state.isLogged.isLoggedIn
+  }
+}
 
 export default Notifications;
